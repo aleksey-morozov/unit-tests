fc683eab67731117e16a3504b9eac3045014df40
Aleksey Morozov <fluentmonk@gmail.com>
2022-03-27 17:08:56 +0400

    fix PR

--- src/services/SquareRootService.ts
+++ src/services/SquareRootService.ts
@@ -1,25 +1,33 @@
 export class SquareRootService {
   public solve(aValue: number, bValue: number, cValue: number) {
-    if (typeof aValue !== 'number' || typeof bValue !== 'number' || typeof cValue !== 'number') {
+    if (
+      !SquareRootService.isNumber(aValue) ||
+      !SquareRootService.isNumber(bValue) ||
+      !SquareRootService.isNumber(cValue)
+    ) {
       throw new Error('Arguments must be number');
     }
 
-    if (aValue === 0) {
+    if (aValue < Number.EPSILON) {
       throw new Error('aValue is 0');
     }
 
     const discriminant = bValue * bValue - 4 * aValue * cValue;
-    if (discriminant < 0) {
+    if (discriminant < -Number.EPSILON) {
       return [];
     }
 
-    if (discriminant > 0) {
+    if (discriminant > Number.EPSILON) {
       const x1 = (-bValue + Math.sqrt(discriminant)) / (2 * aValue);
       const x2 = (-bValue - Math.sqrt(discriminant)) / (2 * aValue);
 
       return [x1, x2];
     }
 
-    return [(-bValue + Math.sqrt(discriminant)) / (2 * aValue)];
+    return [-bValue / (2 * aValue)];
+  }
+
+  private static isNumber(value: any) {
+    return !isNaN(value);
   }
 }
